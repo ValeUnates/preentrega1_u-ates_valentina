cb6b971719fa23ea0c8e6168724ce69f9a082d5a
ValentinaUnates <valeunatessjrc@gmail.com>
2024-07-31 11:41:16 -0300

    SEGUNDA PREENTREGA

--- js/main.js
+++ js/main.js
@@ -1,117 +1,133 @@
-let opciones;
-let nombre= prompt("Ingresa tu nombre");
+let nombre = prompt('Ingrese su nombre').toUpperCase()
 function saludar(){
-    alert("¡Bienvenid@ " + nombre + " a BLURA INDUMENTARIA!");
+    alert(`Bienvenid@ ${nombre} a BLURA INDUMENTARIA`)
 }
 saludar();
 
-const buzos = [
+const listaDeProductos = [
     {
-        color:gris,
-        precio: $18,
-        srock: true,
+        nombreDelProducto: "BUZO LATINA",
+        stock: true,
+        precio: '$150',
+        color: 'GRIS'
     },
     {
-    color: azul,
-    precio: $18,
-    stock: true,
+        nombreDelProducto: "REMERA MARÍA",
+        stock: true,
+        precio: '$100',
+        color: 'BLANCO'
     },
     {
-    color: negro,
-    precio: $20,
-    stock: false,
-    },
-    {
-    color: blanco,
-    precio: $19,
-    stock: true,
-    },
-    {
-    color: rojo,
-    precio: $15,
-    stock: false,
-    },
-]
-const remeras = [
-    {
-    color: gris,
-    precio: $10,
-    stock: false,
-    },
-    {
-    color: azul,
-    precio: $5,
-    stock: true,
-    },
-    {
-    color: negro,
-    precio: $8,
-    stock: false,
-    },
-    {
-    color: blanco,
-    precio: $10,
-    stock: true,
-    },
-    {
-    color: rojo,
-    precio: $9,
-    stock: true,
-    },
-]
-class prod{
-    constructor(color, precio, stock){
-        this.color = color;
-        this.precio= precio;
-        this.stock= stock;
-    }
-}
+        nombreDelProducto: "PANTALÓN MALIBÚ",
+        stock: true,
+        precio: '$350',
+        color: 'BURDEOS'
 
+    },
+    {
+        nombreDelProducto: "SWETER LISBOA",
+        stock: true,
+        precio: '$400',
+        color: 'NEGRO'
+    },
+    {
+        nombreDelProducto: "NIKE AF1",
+        stock: true,
+        precio: '$300',
+        color: 'ROJO'
+    },
+    {
+        nombreDelProducto: "TAPADO LONDRES",
+        stock: true,
+        precio: '$300',
+        color: 'BEIGE'
+    },
+    {
+        nombreDelProducto: "JEAN MOM",
+        stock: false,
+        precio: '$75',
+        color: 'CELESTE'
+    },
+    {
+        nombreDelProducto: "BLAZER TOKYO",
+        stock: true,
+        precio: '$100',
+        color: 'MARRÓN'
+    },
+    {
+        nombreDelProducto: "CALZA LARGA",
+        stock: false,
+        precio: '$150',
+        color: 'MORADO'
+    },
+    {
+        nombreDelProducto: "CALZA 3/4",
+        stock: true,
+        precio: '$200',
+        color: 'BATIK'
+    },
+    {
+        nombreDelProducto: "REMERA NIKE DEPORTIVA",
+        stock: false,
+        precio: '$40',
+        color: 'AMARILLO'
+    },
+    {
+        nombreDelProducto: "CARTERA",
+        stock: true,
+        precio: '$45',
+        color: 'AZUL'
+    },
+];
+class productos {
+    constructor (nombreDelProducto, stock, precio, color){
+        this.nombreDelProducto = nombreDelProducto;
+        this.stock = stock;
+        this.precio = precio;
+        this.color =color;
+    };
+};
 do{
-    opciones = parseInt(prompt(`Menú de opciones:
-        1.Ver Buzos.
-        2.Ver Remeras.
-        3.Para consultar stock.
+    opcion = parseInt(prompt(`Menú de opciones:
+
+        INGRESE UNA OPCION
+        1. Todos los productos
+        2. Stock inmediato
+        3. Buscar producto por nombre
+        4. Buscar producto por color
+
         Para salir, ingrese 0`));
-    switch (opciones) {
+    
+    switch (opcion) {
         case 0:
-            alert("Gracias por tu visita, esperamos tu regreso!");
+            alert ('Gracias por tu visita, esperamos tu regreso!')
             break;
         case 1:
-            let opcionUno = "Buzos:";
-            alert(`${opcionUno} ${buzos} (Aceptar para volver al menú)` );
+            console.log('Lista de productos' ,listaDeProductos)
             break;
+
         case 2:
-            let opcionDos= "Remeras:"
-            alert(`${opcionDos} ${remeras} (Aceptar para volver al menú)`);
-            break;
+            const stock = listaDeProductos.filter (el => el.stock)
+            console.log('El stock de BLURA',stock)
+            alert('En la consola podes ver los productos con entrega inmediata')
+                break;
+
         case 3:
-            function filtrarSock(stock) {
-                if (stock) {
-                    const stock = productos.filter(el => el.stock);
-                    console.log(`Pediste stock de buzos ${buzos.stock}`);
-                    alert("Mirá en la consola el stock disponible");
-                } else {
-                    const sinStock = productos.filter(el => !el.stock);
-                    console.log(`Pediste los proximos ingresos: ${!stock}`);
-                    alert("Mirá en la consola los productos que vamos a sumar al stock");
-                }
-            }
-            case 4:
-            function filtrarSock(stock) {
-                if (stock) {
-                    const stock = productos.filter(el => el.stock);
-                    console.log(`Pediste stock de buzos ${remeras.stock}`);
-                    alert("Mirá en la consola el stock disponible");
-                } else {
-                    const sinStock = productos.filter(el => !el.stock);
-                    console.log(`Pediste los proximos ingresos: ${!stock}`);
-                    alert("Mirá en la consola los productos que vamos a sumar al stock");
-                }
-            }
-            break;
+            const productoABuscar = prompt('Ingrese el nombre del producto que busca').toUpperCase()
+            const nombreDelProducto = listaDeProductos.find(el => el.nombreDelProducto === productoABuscar)
+            console.log('El producto que buscabas es', nombreDelProducto)
+            alert('En la consola está en resultado de tu busqueda')
+                break;
+        
+        case 4:
+            const colorABuscar = prompt('Ingrese el color del producto que busca').toUpperCase()
+            const color = listaDeProductos.find(el => el.color === colorABuscar)
+            console.log('Productos del color que buscabas', color)
+            alert('En la consola está en resultado de tu busqueda')
+                break;
+
         default:
-            alert("Opcion inválida, ingresá una opcion que este dentro del menú de opciones.")
+            alert('Opcion no válida, ingrese la correcta.');
             break;
     }
-}while (opciones!==0);
+} while (opcion !== 0)
